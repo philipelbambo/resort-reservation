2a771a2103aa8a628512a3a58b68dfb1358a4930
philipelbambo <philipyupatelbambo@gmail.com>
2025-04-16 02:26:23 +0800

    this is the final output

--- src/assets/layouts/Header.jsx
+++ src/assets/layouts/Header.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Header = () => {
     return (
-        <header className="bg-gradient-to-r from-[#383e92]  to-[#942e6d] text-white rounded-lg mb-5 shadow-md sticky top-0 z-50">
+        <header className="bg-gradient-to-r from-[#383e92]  to-[#942e6d] text-white rounded-lg mb-5 shadow-md  top-0 z-50">
             <div className="container mx-auto flex justify-between items-center p-6">
             <div className="text-xl font-bold italic">
                Resort Reservation
-            </div>
+           </div>
 
                
             </div>
